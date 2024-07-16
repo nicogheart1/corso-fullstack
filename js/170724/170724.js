0d659b3743b40c9aaebbd8e202a8cc85bc8be2b4
Niccolò Valente <niccolo.valente.biz@gmail.com>
2024-07-16 16:17:56 +0200

    init 1707

---
+++ js/170724/170724.js
@@ -0,0 +1,28 @@
+let altroNome = "Salvatore";
+
+let firstName = "Nick";
+/*
+ codice
+ */
+console.log(firstName);
+/*
+ codice
+ */
+firstName = altroNome + " " + "Mario";
+
+const lastName = "Valente";
+
+
+let age = 18;
+age = false;
+
+firstName = 345;
+
+let faCaldo = true;
+let piove = false;
+
+
+faCaldo = 1;
+piove = 0;
+
+console.log(firstName, lastName, age);
