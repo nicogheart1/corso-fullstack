8dbb764e9dc9e4e81d0abc4dd44588e80b9f558b
Niccolò Valente <niccolo.valente.biz@gmail.com>
2024-09-27 17:09:37 +0200

    270924

---
+++ js/toolchain/270924/src/index.js
@@ -0,0 +1,10 @@
+const moment = require("moment");
+
+const firstName = "Mario";
+
+console.log("Ciao mi chiamo " + firstName);
+
+
+const now = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+console.log(now);
