400f2198ef617bc9cb3b7ba9b39af7a2b121e053
Niccolò Valente <niccolo.valente.biz@gmail.com>
2024-10-16 15:56:22 +0200

    161024 useRef

--- js/react/first-react-app/src/Form.jsx
+++ js/react/first-react-app/src/Form.jsx
@@ -1,20 +1,25 @@
+import { useRef } from "react";
+
 const Form = () => {
+
   const handleOnSubmit = (event) => {
     event.preventDefault();
     console.log("onSubmit", event);
     const data = {
-      email: event.target.elements.email.value,
+      email: emailRef.current.value,
       password: event.target.elements.password.value,
     };
     console.log("form data", data.email, data.password);
   };
+
+  const emailRef = useRef(null);
 
   return (
     <div>
       <h2>Form</h2>
       <div>
         <form onSubmit={handleOnSubmit}>
-          <input type="email" name="email" placeholder="Inssert e-mail" />
+          <input ref={emailRef} type="email" name="email" placeholder="Inssert e-mail" />
           <input
             type="password"
             name="password"
