0f0fe8c5e52712b12c03d1a2074ba7f9be0cb16f
Niccolò Valente <niccolo.valente.biz@gmail.com>
2024-11-05 14:46:18 +0100

    ts config

--- ts/first-react-app-ts/vite.config.ts
+++ ts/first-react-app-ts/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env": env,
+    },
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+      }),
+    ],
+  };
+});
